3ffec050ff7a997afd5f8b849aa71564fdee4258
ChloeGwon <chgmlk@gmail.com>
2022-09-30 17:29:11 -0700

    log-in

---
+++ public/components/LogIn.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '././App.scss';
+
+function LogIn() {
+    return <div></div>;
+//     return <div className='logIn'>
+//         <div className='divInsideLogIn'>
+//             <p className='inputName'>User name</p>
+//             <input name='username' placeholder='' />
+//             <p className='inputName'>Password</p>
+//             <input name='password' placeholder='' type='password' /><br/>
+//             <button>Sign in</button>
+//             <p className='signUpPara'>Sign up</p>
+//         </div>
+//     </div>
+}
+
+export default LogIn
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './App.scss';
 
 function App() {
-	return <div className="App"></div>;
+	return <div className="App">
+		<div className='logIn'>
+			<div className='divInsideLogIn'>
+				<p className='inputName'>User name</p>
+				<input name='username' placeholder='' />
+				<p className='inputName'>Password</p>
+				<input name='password' placeholder='' type='password' /><br/>
+				<button>Sign in</button>
+				<p className='signUpPara'>Sign up</p>
+			</div>
+		</div>
+	</div>;
 }
 
 export default App;
